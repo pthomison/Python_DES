0953ebeff1b5a733e27eaad42e0b6791745cfee8
Patrick <p.thomison@gmail.com>
2015-10-13 14:22:39 -0400

    Finished DES encrypt/decrypt block, starting triple DES

--- data_encryption_standard.py
+++ data_encryption_standard.py
@@ -29,6 +29,18 @@ def key_schedule(key):
 
     return subkeys
 
+def feistel(half_block, subkey):
+    E_R = permutate(half_block, E)
+    XO_E_R = E_R ^ subkey
+    B_tot = BitArray()
+    for y in range(8):
+        B = XO_E_R[y*6:(y+1)*6]
+        row = BitArray([B[0]]+[B[5]]).uint
+        col = BitArray(B[1:5]).uint
+        s_res = BitArray(uint=S[y][row][col], length=4)
+        B_tot.append(s_res)
+    result = permutate(B_tot, P)
+    return result
 
 def des_block_encrypt(input, key):
     if len(input) != 64:
@@ -38,20 +50,46 @@ def des_block_encrypt(input, key):
     subkeys = key_schedule(key)
 
     intial_perm = permutate(input, IP)
-    L = intial_perm[:32]
-    R = intial_perm[32:]
+    L = [None] * 17
+    R = [None] * 17
+    L[0] = intial_perm[:32]
+    R[0] = intial_perm[32:]
 
     for i in range(16):
         iter = i+1
-        E_R = permutate(R, E)
+        L[iter] = R[i]
+        R[iter] = L[i] ^ feistel(R[i], subkeys[i])
 
+    C = permutate(R[16] + L[16], FP)
 
-    return
+    return C
+
+def des_block_decrypt(input, key):
+    if len(input) != 64:
+        raise ValueError("Give inputs of size 64 bits")
+    if len(key) != 64:
+        raise ValueError("Give keys of size 64 bits")
+    subkeys = key_schedule(key)
+    intial_perm = permutate(input, IP)
+    L = [None] * 17
+    R = [None] * 17
+    L[16] = intial_perm[32:]
+    R[16] = intial_perm[:32]
+
+    for i in range(16).__reversed__():
+        iter = i+1
+        R[iter-1]=L[iter]
+        L[iter-1]=R[iter] ^ feistel(L[iter], subkeys[i])
+
+    PB = permutate(L[0]+R[0], FP)
+
+    return PB
+
 
 
 
 if __name__ == "__main__":
-    input = BitArray(int=7000, length=64)
-    #key = BitArray(int=9379, length=64)
-    key = BitArray(bin="00010011 00110100 01010111 01111001 10011011 10111100 11011111 11110001")
+    input = BitArray(int=12, length=64)
+    key = BitArray(uint=9397, length=64)
     result = des_block_encrypt(input, key)
+    print(des_block_decrypt(result, key).uint)
